ba628c0dca9eb192cac478e577fcba8846ca10d9
Dave Leaver <dave@torutek.com>
2025-09-10 14:16:35 +1200

    Remove unmaintained SigSpec.AspNetCore

--- src/SigSpec.AspNetCore/SigSpecSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SigSpec.AspNetCore.Middlewares
-{
-    public class SigSpecSettings
-    {
-        /// <summary>Gets or sets the path to serve the SigSpec document (default: '/sigspec/{documentName}/sigspec.json').</summary>
-        public string Path { get; set; } = "/sigspec/{documentName}/sigspec.json";
-    }
-}
--- src/SigSpec.AspNetCore/SigSpecUiSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SigSpec.AspNetCore
-{
-    public class SigSpecUiSettings
-    {
-        public string Route { get; set; } = "/sigspec";
-    }
-}
